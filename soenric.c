6a04a49d7e286816aa08a487c672e8e63cdc582c
enricfelixp2019 <58304229+enricfelixp2019@users.noreply.github.com>
2019-12-02 14:31:37 -0300

    Update soenric.c

--- soenric.c
+++ soenric.c
@@ -13,18 +13,19 @@ struct c {
 int saldo;
 };
 typedef struct c conta;
-conta from, to;
+conta from, to,aux;
 int valor;
 // The child thread will execute this function
 
 int transferencia_deposito( void *threadarg)
-{
-
+{pthread_mutex_t mutexsum;
+  for (int i = 0; i < 50; i++) {
     if (from.saldo >= valor){
-
-   from.saldo -= valor;
-    to.saldo += valor;
-
+  pthread_mutex_lock (&mutexsum);
+   from.saldo -= to.saldo;
+    to.saldo += from.saldo;
+  pthread_mutex_unlock (&mutexsum);
+    }
     }
 
  printf("Transferência concluída com sucesso!\n");
@@ -34,29 +35,31 @@ printf("Saldo de c2: %d\n", to.saldo);
  pthread_exit(0);
 
 }
+
 int transferencia_inversa( void *threadarg)
-{
-
-    if (to.saldo >+ from.saldo){
-
-    to.saldo -= valor;
-    from.saldo += valor; 
+{pthread_mutex_t mutexsum;
+  for (int j = 0; j < 50; j++) {
+    if (to.saldo>=from.saldo){
+  pthread_mutex_lock (&mutexsum);
+   to.saldo -= from.saldo;
+    from.saldo += to.saldo;
+  pthread_mutex_unlock (&mutexsum);
+    }
     }
 
- printf("Transferência concluída com sucesso!\n");
+printf("Transferência concluída com sucesso!\n");
 printf("Saldo de c1: %d\n", from.saldo);
 printf("Saldo de c2: %d\n", to.saldo);
  return 0;
  pthread_exit(0);
 
 }
-
 
 
 int main()
 {
 
-pthread_mutex_t mutexsum;
+
 
  void* stack;
 pid_t pid;
@@ -64,40 +67,35 @@ pid_t pid;
 
 from.saldo = 100;
  to.saldo = 100;
+
  printf( "Transferindo 1 para a conta c2\n" );
 valor = 1;
 
-int numero=rand()%2;
-    if (numero==1){
-            for (i = 0; i < 100; i++) {
+
+            
             pthread_t t1,t2;
 
 
             pthread_create (&t1, NULL,(void*)transferencia_deposito, NULL);
-            pthread_mutex_lock (&mutexsum);
+            
 
             perror( "clone" );
-            pthread_mutex_unlock (&mutexsum);
+            
             pthread_join (t1, NULL);
             
 
             pthread_create (&t2, NULL,(void*)transferencia_inversa, NULL);
-            pthread_mutex_lock (&mutexsum);
+          
 
             perror( "clone" );
-            pthread_mutex_unlock (&mutexsum);
+        
             pthread_join (t2, NULL);
 
     return 0;
             
             
             
-            }
+            
 
         printf("Transferências concluídas e memória liberada.\n");
     }
-
-
-
-
-                    }
